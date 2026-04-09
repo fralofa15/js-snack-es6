871dbb475d38b754e0cb26c85a789cbd74b85c31
Lorenzo Favale <lorenzofavale150420@gmail.com>
2026-04-09 16:36:00 +0200

    Creazione array di oggetti

---
+++ index-2.js
@@ -0,0 +1,38 @@
+//Array di oggetti con nome punti e falli
+const teams = [
+    {
+        nome: "Juventus",
+        punti: 0,
+        falli: 0
+    },
+    {
+        nome: "Roma",
+        punti: 0,
+        falli: 0
+    },
+    {
+        nome: "Milan",
+        punti: 0,
+        falli: 0
+    },
+    {
+        nome: "Inter",
+        punti: 0,
+        falli: 0
+    },
+    {
+        nome: "Fiorentina",
+        punti: 0,
+        falli: 0
+    },
+    {
+        nome: "Lazio",
+        punti: 0,
+        falli: 0
+    },
+    {
+        nome: "Napoli",
+        punti: 0,
+        falli: 0
+    }
+];
