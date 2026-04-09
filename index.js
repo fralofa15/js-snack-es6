43963c876f9f27495a774f10501a8cefffb7fd10
Lorenzo Favale <lorenzofavale150420@gmail.com>
2026-04-09 16:29:41 +0200

    Creazione di un ciclo for per controllare la bici più leggera

--- index.js
+++ index.js
@@ -17,3 +17,19 @@ const bici = [
         peso: 9
     }
 ];
+
+let biciLeggera = bici[0];
+
+
+//Ciclo for per trovare la bici più leggera
+for (let i = 1; i < bici.length; i++) {
+    console.log(bici[i]);
+    
+    //Controllo tramite IF quale bici ha il peso minore
+    if (bici[i].peso < biciLeggera.peso) {
+        //Se la nuova bici è più leggera aggiorno la variabile biciLeggera
+        biciLeggera = bici[i]; 
+    }
+}
+
+console.log(biciLeggera);
