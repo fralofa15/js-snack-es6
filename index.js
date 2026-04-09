5c0549955a18d8702ab6a2ec2bd19082e982a13c
Lorenzo Favale <lorenzofavale150420@gmail.com>
2026-04-09 16:11:49 +0200

    Creazione dell'array di oggetti

---
+++ index.js
@@ -0,0 +1,19 @@
+//Array di oggetti con nome e peso
+const bici = [
+    {
+        nome: "Bicicletta 1", 
+        peso: 7
+    },
+    {
+        nome: "Bicicletta 2", 
+        peso: 6
+    },
+    {
+        nome: "Bicicletta 3", 
+        peso: 12
+    },
+    {
+        nome: "Bicicletta 4", 
+        peso: 9
+    }
+];
